3f957d449b2e942b18509ab7697bd038b118f155
Chris Rama <62946196+chrisrama@users.noreply.github.com>
2021-04-29 17:09:48 +0200

    Update Header.js
    added some note

--- src/component/Header.js
+++ src/component/Header.js
@@ -4,7 +4,7 @@ import react from "react";
 class Header extends react.component {
 
    render(){
-     return (<div><h1> `header` </h1> </div>);
+     return (<div><h1> `header` let see if it work </h1> </div>);
    }
 }
 export default Header;
